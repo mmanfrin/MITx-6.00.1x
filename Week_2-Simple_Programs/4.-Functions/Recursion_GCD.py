fca2443b2b7d3c8f44149e87143e3e8af6fa0625
mmanfrin <40899027+mmanfrin@users.noreply.github.com>
2020-02-06 16:57:32 -0300

    End Week 2

---
+++ Week_2-Simple_Programs/4.-Functions/Recursion_GCD.py
@@ -0,0 +1,39 @@
+# Exercise: gcd recur
+
+# (5/5 points)
+# ESTIMATED TIME TO COMPLETE: 6 minutes
+
+# The greatest common divisor of two positive integers is the largest integer that divides each of them without remainder. For example,
+# gcd(2, 12) = 2
+# gcd(6, 12) = 6
+# gcd(9, 12) = 3
+# gcd(17, 12) = 1
+
+# A clever mathematical trick (due to Euclid) makes it easy to find greatest common divisors. Suppose that a and b are two positive
+# integers:
+# If b = 0, then the answer is a
+# Otherwise, gcd(a, b) is the same as gcd(b, a % b)
+
+# Write a function gcdRecur(a, b) that implements this idea recursively. This function takes in two positive integers and returns one
+# integer.
+
+def gcdRecur(a, b):
+    """search fod the GCD
+    
+    Arguments:
+        a {int} -- 1st number
+        b {int} -- 2nd number
+    """    
+    # Your code here
+    
+    if a == 0:
+        return b
+    elif b == 0:
+        return a
+    else:
+        return gcdRecur(b, a % b)
+    
+    
+a = 56
+b = 16
+print(gcdRecur(a, b))
---
+++ Week_2-Simple_Programs/blergh.py
@@ -0,0 +1,11 @@
+# x = 176
+# y = round(x / 10.0) * 10
+
+balance = 3329
+annualInterestRate = 0.2
+
+originalBalance = balance
+InterestRate = annualInterestRate / 12
+payment = 10
+
+balance = 
