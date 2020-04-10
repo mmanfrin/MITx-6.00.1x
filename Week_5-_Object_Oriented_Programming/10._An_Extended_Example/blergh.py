03b415ecd63adbc25124294bdd71c5c1d46dd022
Marcos Manfrin <mmanfrin2@gmail.com>
2020-04-10 11:03:55 -0300

    .

--- Week_5-_Object_Oriented_Programming/10._An_Extended_Example/blergh.py
+++ Week_5-_Object_Oriented_Programming/10._An_Extended_Example/blergh.py
@@ -1,25 +1,8 @@
-class Person(object):
-    def __init__(self, name, birthday=None, teste=None):
-        self.name = name
-        self.birthday = birthday
-        self.teste = teste
-        self.last_name = name.split(' ')[-1]
+import string
 
-    def get_last_name(self):
-        return self.last_name
-
-    def __str__(self):
-        return 'Nome: ' + self.last_name + ', ' + self.name
-
-    def __repr__(self):
-
-
-marcos = Person('Marcos Manfrin', None, 1)
-
-print(marcos)
-print(marcos.last_name)
-print(marcos.birthday)
-print(marcos.teste)
-
-
-print('.')
+# k = int(input('entre a key: '))
+k = 250
+# enc = int((k/26)+1)
+# alph = str(string.ascii_lowercase) * enc
+dic = str(string.ascii_lowercase) * int((k/26)+1)
+print(len(alph))
---
+++ Week_5-_Object_Oriented_Programming/Problem_Set_5/blergh.py
@@ -0,0 +1,7 @@
+def decrypt_story():
+    joke_code = CiphertextMessage(get_story_string())
+    return joke_code.decrypt_message()
+
+def decrypt_story():
+    cipher_story = CiphertextMessage(get_story_string())
+    return cipher_story.decrypt_message()
